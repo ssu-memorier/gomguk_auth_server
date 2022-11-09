e03bb8b3f4030a95a36b6d7e0d89a15a5aba44b4
Tuna-is-DW <ydw5210@gmail.com>
2022-11-10 01:27:55 +0900

    chore/추후 개발할 부분 주석남김

--- src/routes/req.js
+++ src/routes/req.js
@@ -4,7 +4,7 @@ const { isLoggedIn } = require("./loginCheckMiddlewares");
 const router = express.Router();
 
 router.get("/", isLoggedIn, (req, res) => {
-  res.send("로그인 상태입니다.");
+  res.send("로그인 상태입니다."); //유저 요청 전송할 부분
 });
 
 module.exports = router;
