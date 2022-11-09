91d7a48a690266efe85bad3a10052582f488f81c
Tuna-is-DW <ydw5210@gmail.com>
2022-11-10 00:55:07 +0900

    feature/로그인 시 동작 틀 생성

---
+++ src/routes/req.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { isLoggedIn } = require("./loginCheckMiddlewares");
+
+const router = express.Router();
+
+router.get("/", isLoggedIn, (req, res) => {
+  res.send("로그인 상태입니다.");
+});
+
+module.exports = router;
