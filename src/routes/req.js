2ef46bb7c9fb8d05f7d2b9807c13a145f617ef3b
Tuna-is-DW <ydw5210@gmail.com>
2022-11-10 22:23:57 +0900

    refactor/구조변경

--- src/routes/req.js
+++ src/routes/req.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const { isLoggedIn } = require("./loginCheckMiddlewares");
+const express = require('express');
+const { isLoggedIn } = require('./middlewares/loginCheckMiddleware');
 
 const router = express.Router();
 
-router.get("/", isLoggedIn, (req, res) => {
-  res.send("로그인 상태입니다."); //유저 요청 전송할 부분
+router.get('/', isLoggedIn, (req, res) => {
+    res.send('로그인 상태입니다.'); //유저 요청 전송할 부분
 });
 
 module.exports = router;
