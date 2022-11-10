d0f927460c9fd5ec49efa323a7be1374b6a8b7b2
Tuna-is-DW <ydw5210@gmail.com>
2022-11-10 12:19:33 +0900

    feature/view관련 파일 및 코드삭제

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
-});
-
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("main", { title: "홈" });
+    res.locals.user = req.user;
+    next();
 });
 
 module.exports = router;
