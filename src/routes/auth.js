56c9e59c01fc1d516696bdc932955f68ef13a2de
Tuna-is-DW <ydw5210@gmail.com>
2022-11-15 23:54:42 +0900

    feature/로그아웃 라우터 통합 후 provider 별로 분리

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,11 +3,10 @@ const passport = require('passport');
 const jwt = require('../../modules/jwt');
 const User = require('../../models/user');
 const logoutModules = require('../../modules/oauth/logout');
-const { isTokenValid } = require('./middlewares/tokenValidateMiddleware');
 
 const router = express.Router();
 
-router.get('/profile', isTokenValid, async (req, res) => {
+router.get('/profile', async (req, res) => {
     try {
         const payload = await jwt.getPayload(req.cookies.jwt.token);
         const user = await User.findOne({
@@ -44,9 +43,14 @@ router.get(
         }
     }
 );
-router.get('/kakao/logout', isTokenValid, async (req, res) => {
+
+router.get('/logout', async (req, res) => {
     const jwtPayload = await getPayload(req);
-    logoutModules.kakao(jwtPayload.accessToken);
+    if (jwtPayload.provider === 'kakao') {
+        logoutModules.kakao(jwtPayload.accessToken);
+    } else {
+        logoutModules.google(jwtPayload.accessToken);
+    }
     res.cookie('jwt', null, { maxAge: 0 });
     req.logout(() => {
         res.redirect('/');
@@ -72,17 +76,10 @@ router.get(
         }).redirect('/');
     }
 );
-router.get('/google/logout', isTokenValid, async (req, res) => {
-    const jwtPayload = await getPayload(req);
-    logoutModules.google(jwtPayload.accessToken);
-    res.cookie('jwt', null, { maxAge: 0 });
-    req.logout(() => {
-        res.redirect('/');
-    });
-});
 
 const getPayload = async (req) => {
-    const [_, payload] = await jwt.verify(req.cookies.jwt.token);
+    console.log(req.cookies.jwt.token);
+    const payload = await jwt.getPayload(req.cookies.jwt.token);
     return payload;
 };
 
