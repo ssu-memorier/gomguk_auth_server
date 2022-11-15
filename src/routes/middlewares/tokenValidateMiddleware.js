1d068e5589bfd836773ee10328b5938e6420b306
Tuna-is-DW <ydw5210@gmail.com>
2022-11-15 23:29:04 +0900

    feature/토큰 재발급 로직 구현

--- src/routes/middlewares/tokenValidateMiddleware.js
+++ src/routes/middlewares/tokenValidateMiddleware.js
@@ -6,15 +6,28 @@ const {
 } = require('../../constants/tokenVerifyState');
 
 exports.isTokenValid = async (req, res, next) => {
-    const jwtToken = req.cookies.jwt.token;
-
-    const isValidToken = await jwt.verify(jwtToken);
-    if (isValidToken[0] === TOKEN_VERIFIED) {
-        next();
-    } else {
-        if (isValidToken[0] === TOKEN_EXPIRED)
-            res.status(401).send('expired token');
-        else if (isValidToken[0] === TOKEN_INVALID)
-            res.status(401).send('invalid token');
+    try {
+        const jwtToken = req.cookies.jwt.token;
+        const verifyState = await jwt.verify(jwtToken);
+        if (verifyState === TOKEN_VERIFIED) {
+            next();
+        } else {
+            if (verifyState === TOKEN_EXPIRED) {
+                //jwt 재발급
+                res.cookie('jwt', null, { maxAge: 0 });
+                const newJwtToken = await jwt.issue(payload);
+                res.cookie('jwt', newJwtToken, {
+                    httpOnly: true,
+                    secure: true,
+                });
+                next();
+            } else if (verifyState === TOKEN_INVALID)
+                res.status(401).json({
+                    code: 401,
+                    msg: 'unauthorized',
+                });
+        }
+    } catch (err) {
+        console.log(err);
     }
 };
