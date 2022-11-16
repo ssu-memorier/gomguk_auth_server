e08f9d2dc29c1a8669c3b85552f41c57dc76e100
Tuna-is-DW <ydw5210@gmail.com>
2022-11-17 00:38:49 +0900

    feature/토큰 검증 시 만료 여부 파악하고 한시간 이내일시 재발급

--- src/routes/middlewares/tokenValidateMiddleware.js
+++ src/routes/middlewares/tokenValidateMiddleware.js
@@ -4,23 +4,39 @@ const {
     TOKEN_INVALID,
     TOKEN_VERIFIED,
 } = require('../../constants/tokenVerifyState');
+const { ONE_HOUR } = require('../../constants/tokenTime');
+
+const checkExpired = async (res, payload) => {
+    const currentTime = Math.floor(new Date().getTime() / 1000);
+    const leftTime = payload.exp - currentTime;
+
+    if (leftTime < ONE_HOUR) {
+        console.log('재발급');
+        res.cookie('jwt', null, { maxAge: 0 });
+        const newJwtToken = await jwt.issue(payload);
+        res.cookie('jwt', newJwtToken, {
+            httpOnly: true,
+            secure: true,
+        });
+        return;
+    }
+    return;
+};
 
 exports.isTokenValid = async (req, res, next) => {
     try {
         const jwtToken = req.cookies.jwt.token;
         const verifyState = await jwt.verify(jwtToken);
         if (verifyState === TOKEN_VERIFIED) {
+            const payload = await jwt.getPayload(jwtToken);
+            await checkExpired(res, payload);
             next();
         } else {
             if (verifyState === TOKEN_EXPIRED) {
-                //jwt 재발급
                 res.cookie('jwt', null, { maxAge: 0 });
-                const newJwtToken = await jwt.issue(payload);
-                res.cookie('jwt', newJwtToken, {
-                    httpOnly: true,
-                    secure: true,
+                req.logout(() => {
+                    res.redirect('/');
                 });
-                next();
             } else if (verifyState === TOKEN_INVALID)
                 res.status(401).json({
                     code: 401,
