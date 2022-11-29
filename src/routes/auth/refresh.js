61e12bca7dbf585dcf5369d6f55bc1841578366a
Tuna-is-DW <ydw5210@gmail.com>
2022-11-29 20:09:12 +0900

    feature/refresh토큰 만료된 경우 에러 반환

--- src/routes/auth/refresh.js
+++ src/routes/auth/refresh.js
@@ -21,35 +21,45 @@ router.get('/', async (req, res) => {
     const refreshToken = user.refreshToken;
     const provider = user.provider;
     if (provider === 'kakao') {
-        const newTokenData = await getNewTokenFromKakao(refreshToken);
-        const [newAT, newRT] = [
-            newTokenData.access_token,
-            newTokenData.refresh_token,
-        ];
-        await user.update({ accessToken: newAT, refreshToken: newRT });
+        try {
+            const newTokenData = await getNewTokenFromKakao(refreshToken);
+            const [newAT, newRT] = [
+                newTokenData.access_token,
+                newTokenData.refresh_token,
+            ];
+            await user.update({ accessToken: newAT, refreshToken: newRT });
+        } catch (err) {
+            res.status(401).json({
+                code: 401,
+                message: 'REFRESH_LOGIN',
+            });
+            return;
+        }
     } else {
-        const newTokenData = await getNewTokenFromGoogle(refreshToken);
-        const newAT = newTokenData.access_token;
-        await user.update({ accessToken: newAT });
+        try {
+            const newTokenData = await getNewTokenFromGoogle(refreshToken);
+            const newAT = newTokenData.access_token;
+            await user.update({ accessToken: newAT });
+        } catch (err) {
+            res.status(401).json({
+                code: 401,
+                message: 'REFRESH_LOGIN',
+            });
+            return;
+        }
     }
-    try {
-        const newJWT = await issueNewJWT(user);
-        res.cookie(JWT, null, { domain: '.paas-ta.org', maxAge: 0 });
-        res.cookie(JWT, newJWT, {
-            domain: '.paas-ta.org',
-            httpOnly: true,
-            secure: true,
-        });
-        res.status(200).json({
-            code: 200,
-            msg: 'token refresh success',
-        });
-    } catch (err) {
-        res.status(500).json({
-            code: 500,
-            msg: 'token refresh failed',
-        });
-    }
+
+    const newJWT = await issueNewJWT(user);
+    res.cookie(JWT, null, { domain: '.paas-ta.org', maxAge: 0 });
+    res.cookie(JWT, newJWT, {
+        domain: '.paas-ta.org',
+        httpOnly: true,
+        secure: true,
+    });
+    res.status(200).json({
+        code: 200,
+        msg: 'token refresh success',
+    });
 });
 
 module.exports = router;
